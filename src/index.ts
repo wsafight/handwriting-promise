8dbb52f932439a3c84438eff03a9fba9e2f836df
ShianWang <984292420@qq.com>
2021-03-03 21:03:12 +0800

    feature: add state props

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 
 const constantFun = (value: any) => value
+
 const errorFun = (reason: any) => {
   throw reason
 }
 
+type PromiseState = 'pending' | 'resolved' | 'rejected'
 
 export default class MyPromise<T> {
+  state: PromiseState | undefined  = undefined;
+  value: T | undefined = undefined
+  reason: any = undefined
+  onResolvedTodoList = []
+  onRejectedTodoList = []
+
 
   constructor(excutor: any) {
     console.log(excutor)
@@ -18,8 +26,7 @@ export default class MyPromise<T> {
 
   }
 
-  onResolvedTodoList = []
-  onRejectedTodoList = []
+
 
   then(onResolve: any = constantFun, onRejected: any = errorFun) {
 
