bc096e4bf4dc76d7538940abe945cb6662bbe1db
ShianWang <984292420@qq.com>
2021-03-03 21:26:19 +0800

    feature: Implement resolve reject fun

--- src/index.ts
+++ src/index.ts
@@ -11,29 +11,57 @@ const enum MyPromiseState {
   Rejected = 'rejected'
 }
 
+/**
+ * Asynchronous Container
+ * The asynchronous operation entrusts the execution result
+ */
 export default class MyPromise<T> {
+  // current state
   state: MyPromiseState | undefined  = undefined;
+
   value: T | undefined = undefined
   reason: any = undefined
-  onResolvedTodoList = []
-  onRejectedTodoList = []
+  onResolvedTodoList: Function[] = []
+  onRejectedTodoList: Function[] = []
 
 
   constructor(excutor: any) {
-    console.log(excutor)
+    try {
+      excutor(this.resolve, this.reject)
+      this.state = MyPromiseState.Pending
+    }catch (e) {
+      this.reject(e)
+    }
   }
 
   resolve = (value: T) => {
-
+    if (this.state !== MyPromiseState.Pending) {
+      return
+    }
+    this.state = MyPromiseState.Resolved
+    this.value = value
+    while (this.onResolvedTodoList.length) {
+      const fun = this.onResolvedTodoList.shift()
+      typeof fun === 'function' && fun()
+    }
   }
-  reject = (reason: any) => {
 
+  reject = (reason: any) => {
+    if (this.state !== MyPromiseState.Pending) {
+      return
+    }
+    this.state = MyPromiseState.Rejected
+    this.reason = reason
+    while (this.onRejectedTodoList.length) {
+      const fun = this.onRejectedTodoList.shift()
+      typeof fun === 'function' && fun()
+    }
   }
 
 
 
   then(onResolve: any = constantFun, onRejected: any = errorFun) {
-
+    console.log(onResolve, onRejected)
   }
 
 }
